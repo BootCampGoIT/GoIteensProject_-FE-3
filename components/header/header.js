5c6a8f6b9a5d6df50cdd8afe2763b374f726d0fe
AndriiShnurkov <Shans12.84@ukr.net>
2020-05-17 12:49:20 +0300

    added listItems

--- components/header/header.js
+++ components/header/header.js
@@ -1,10 +1,17 @@
+import { createMarkup } from '../pages/games.js';
+import { games, programs } from '../../data.js';
+
+
 const header = () => {
     const navigationList = document.querySelector('.navigationList'); //ul
+    const listItems = document.querySelector('.listItems'); //ul
+
+
     const user = document.querySelector('.user');
 
     const currentActivePage = document.querySelector('.activeLink'); //a
     const currentPageName = document.querySelector('.currentPageName'); //p
-    currentPageName.textContent = currentActivePage.dataset.page;
+    listItems.innerHTML = createMarkup(games);
 
 
     const setActiveLink = (event) => {
@@ -15,15 +22,32 @@ const header = () => {
         currentActiveLink && currentActiveLink.classList.remove('activeLink');
         event.target.classList.add('activeLink');
 
-        currentPageName.textContent = event.target.dataset.page;
+        switch (event.target.dataset.page) {
+            case 'games':
+                listItems.innerHTML = createMarkup(games);
+                break;
+            case 'programs':
+                listItems.innerHTML = createMarkup(programs)
+                break;
+            case 'news':
+                listItems.innerHTML = createMarkup(programs)
+                break;
+            default:
+                break;
+        }
     }
 
     const setUser = (event) => {
         event.currentTarget.classList.toggle('activeUser');
     }
 
+    const addToCart = (event) => {
+        console.log(event.target)
+    }
+
     navigationList.addEventListener('click', setActiveLink);
     user.addEventListener('click', setUser);
+    listItems.addEventListener('click', addToCart)
 
 }
 
